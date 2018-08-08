9cf177642cfc65cd042bb5ecc56ec54b9f7daf9d
Luis Castillo <luiscastillo@iwebsapp.com>
2018-08-08 03:13:16 -0500

    test agent

---
+++ platziverse-db/test/agent-test.js
@@ -0,0 +1,17 @@
+'use stric'
+
+const test = require('ava')
+let db = null
+
+let config = {
+  loggin: function () {}
+}
+
+test.beforeEach(async () => {
+  const setupDataBase = require('../')
+  db = await setupDataBase(config)
+})
+
+test('Agent', t => {
+  t.truthy(db.Agent, 'Agent sure exit')
+})
---
+++ platziverse-db/tests/agent-test.js
@@ -0,0 +1,7 @@
+'use stric'
+
+const test = require('ava')
+
+test('Make it pass', t => {
+  t.pass()
+})
