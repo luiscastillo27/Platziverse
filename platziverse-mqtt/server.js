19bb1c718ce202fd60cadf2f504410fb32853259
Luis Castillo <luiscastillo@iwebsapp.com>
2018-08-09 02:29:50 -0500

    integrate db

--- platziverse-mqtt/server.js
+++ platziverse-mqtt/server.js
@@ -4,11 +4,21 @@ const debug = require('debug')('platziverse:mqtt')
 const mosca = require('mosca')
 const redis = require('redis')
 const chalk = require('chalk')
+const db = require('platziverse-db')
 
 const backend = {
   type: 'redis',
   redis,
   return_buffers: true
+}
+
+const config = {
+  database: process.env.DB_NAME || 'platziverse',
+  username: process.env.DB_USER || 'luis',
+  password: process.env.DB_PASS || 'jimyluis',
+  host: process.env.DB_HOST || 'localhost',
+  dialect: 'postgres',
+  logging: s => debug(s)
 }
 
 const settings = {
@@ -17,6 +27,8 @@ const settings = {
 }
 
 const server = new mosca.Server(settings)
+
+let Agent, Metric
 
 server.on('clientConnected', client => {
   debug(`Client connected: ${client.id}`)
@@ -31,7 +43,10 @@ server.on('published', (packet, client) => {
   debug(`Payload: ${packet.payload}`)
 })
 
-server.on('ready', function(){
+server.on('ready', async function () {
+  const services = await db(config).catch(handleFatalError)
+  Agent = services.Agent
+  Metric = services.Metric
   console.log(`${chalk.green('[platziverse-mqtt]')} server is running`)
 })
 
@@ -39,6 +54,6 @@ server.on('error', handleFatalError)
 process.on('uncaughtException', handleFatalError)
 process.on('unhandledRejection', handleFatalError)
 
-function handleFatalError(err){
+function handleFatalError (err) {
   console.error(`${chalk.red('[Fatal error]')} ${err.message}`)
 }
