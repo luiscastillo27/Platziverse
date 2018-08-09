8ee881db1804e49a4b55ccb3a741f1f4f4f1ee64
Luis Castillo <luiscastillo@iwebsapp.com>
2018-08-09 02:47:19 -0500

    report agent connected

--- platziverse-mqtt/server.js
+++ platziverse-mqtt/server.js
@@ -5,6 +5,7 @@ const mosca = require('mosca')
 const redis = require('redis')
 const chalk = require('chalk')
 const db = require('platziverse-db')
+const { parsePayload } = require('./utils')
 
 const backend = {
   type: 'redis',
@@ -27,20 +28,57 @@ const settings = {
 }
 
 const server = new mosca.Server(settings)
-
+const clients = new Map()
 let Agent, Metric
 
 server.on('clientConnected', client => {
   debug(`Client connected: ${client.id}`)
+  clients.set(client.id, null)
 })
 
 server.on('clientDisconnected', client => {
   debug(`Client disconnected: ${client.id}`)
 })
 
-server.on('published', (packet, client) => {
+server.on('published', async (packet, client) => {
   debug(`Received: ${packet.topic}`)
-  debug(`Payload: ${packet.payload}`)
+  switch (packet.topic) {
+    case 'agent/connected':
+    case 'agent/disconnected':
+      debug(`Payload: ${packet.payload}`)
+      break
+    case 'agent/message':
+      debug(`Payload: ${packet.payload}`)
+      const payload = parsePayload(packet.payload)
+      if (payload) {
+        payload.agent.connected = true
+        let agent
+        try {
+          agent = await Agent.createOrUpdate(payload.agent)
+        } catch (e) {
+          return handleError(e)
+        }
+
+        debug(`Agent ${agent.uuid} saved`)
+        // notify Agent is connected
+        if (!client.get(client.id)) {
+          clients.set(client.id, agent)
+          server.publish({
+            'topic': 'agent/connected',
+            payload: JSON.stringify({
+              agent: {
+                uuid: agent.uuid,
+                name: agent.name,
+                hostname: agent.hostname,
+                pid: agent.pid,
+                connected: agent.connected
+              }
+            })
+          })
+        }
+      }
+      break
+  }
 })
 
 server.on('ready', async function () {
@@ -56,4 +94,11 @@ process.on('unhandledRejection', handleFatalError)
 
 function handleFatalError (err) {
   console.error(`${chalk.red('[Fatal error]')} ${err.message}`)
+  console.error(err.stack)
+  process.exit(1)
+}
+
+function handleError (err) {
+  console.error(`${chalk.red('[Error]')} ${err.message}`)
+  console.error(err.stack)
 }
