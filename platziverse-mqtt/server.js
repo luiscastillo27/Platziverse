c3e239f053797e75b7b70a02cc99d8c7fe27560a
Luis Castillo <luiscastillo@iwebsapp.com>
2018-08-09 02:21:18 -0500

    start server

---
+++ platziverse-mqtt/server.js
@@ -0,0 +1,44 @@
+'use stric'
+
+const debug = require('debug')('platziverse:mqtt')
+const mosca = require('mosca')
+const redis = require('redis')
+const chalk = require('chalk')
+
+const backend = {
+  type: 'redis',
+  redis,
+  return_buffers: true
+}
+
+const settings = {
+  port: 1883,
+  backend
+}
+
+const server = new mosca.Server(settings)
+
+server.on('clientConnected', client => {
+  debug(`Client connected: ${client.id}`)
+})
+
+server.on('clientDisconnected', client => {
+  debug(`Client disconnected: ${client.id}`)
+})
+
+server.on('published', (packet, client) => {
+  debug(`Received: ${packet.topic}`)
+  debug(`Payload: ${packet.payload}`)
+})
+
+server.on('ready', function(){
+  console.log(`${chalk.green('[platziverse-mqtt]')} server is running`)
+})
+
+server.on('error', handleFatalError)
+process.on('uncaughtException', handleFatalError)
+process.on('unhandledRejection', handleFatalError)
+
+function handleFatalError(err){
+  console.error(`${chalk.red('[Fatal error]')} ${err.message}`)
+}
