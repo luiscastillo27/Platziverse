4849a3e47b4108e3856fbc2d6962fc689444ab88
Luis Castillo <luiscastillo@iwebsapp.com>
2018-08-09 23:04:53 -0500

    routes api

--- platziverse-api/server.js
+++ platziverse-api/server.js
@@ -3,9 +3,13 @@
 const http = require('http')
 const express = require('express')
 const chalk = require('chalk')
+const api = require('./api')
+
 const port = process.env.PORT || 2715
 const app = express()
 const server = http.createServer(app)
+
+app.use('/api', api)
 
 server.listen(port, () => {
   console.log(`${chalk.green('[platziverse-api]')} server listeing on port ${port}`)
